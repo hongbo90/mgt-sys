34c37f8fe5427088fd55680b62e115caa555b5b9
Eversleeping <568031119@qq.com>
2017-04-19 21:36:08 +0800

    antd index

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,17 +7,9 @@ class Home extends React.Component{
 	render(){
 		return (
 			<HomeLayout title="Welcome">
-
-				<main>
-					<Link to="/user/add">添加用户</Link>
-					<br />
-					<Link to="/user/list">用户列表</Link>
-					<br />
-					<Link to="/book/add">添加图书</Link>
-					<br />
-					<Link to="/book/list">图书列表</Link>
-				</main>
-
+				<div className={style.welcome}>
+					Welcome
+				</div>
 			</HomeLayout>
 		);
 	}
