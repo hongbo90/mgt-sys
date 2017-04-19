c7b686df1ca9b75503a243e4c4ad878740e83525
Eversleeping <568031119@qq.com>
2017-04-19 21:31:48 +0800

    antd add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import HomeLayout from '../layout/HomeLayout';
+import style from '../styles/home-page.less';
 
 class Home extends React.Component{
 	render(){
